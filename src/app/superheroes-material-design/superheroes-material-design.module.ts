7ed85b69710646652504c6614052f06f142f4f1c
Hans Naert <u0081767@vives.be>
2023-09-21 17:00:58 +0200

    Add super-heroes-material-design module

---
+++ src/app/superheroes-material-design/superheroes-material-design.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule
+  ]
+})
+export class SuperheroesMaterialDesignModule { }
