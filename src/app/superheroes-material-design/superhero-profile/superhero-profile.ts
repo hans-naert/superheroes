a5419a128423f37520266a4c221b8ec68cc648ab
Hans Naert <hans.naert@vives.be>
2025-10-17 12:08:13 +0200

    Use component inputs

--- src/app/superheroes-material-design/superhero-profile/superhero-profile.ts
+++ src/app/superheroes-material-design/superhero-profile/superhero-profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import SuperheroText from '../utilities/superhero-text';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-superhero-profile',
@@ -12,5 +13,24 @@ import SuperheroText from '../utilities/superhero-text';
   providers: [SuperheroText]
 })
 export class SuperheroProfile {
+
+   @Input() name: string="";
+   //@Input() firstAppearance: string="";
+   @Input("lives-in") livesIn: string="";
+
+    // create a private field 
+    private _firstAppearance: number=0; 
+    // use a public setter 
+    @Input() set firstAppearance(val: number) 
+    { if (val > 1950)
+      { this._firstAppearance = val;   } 
+      else 
+        { console.error("Superhero is too old"); 
+
+        } 
+      } 
+      // getter for the use of template. 
+      get firstAppearance() 
+      { return this._firstAppearance; }
 
 }
