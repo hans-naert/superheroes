8d7e572b631ec686206891085d419f312e4e625e
Hans Naert <hans.naert@vives.be>
2025-10-17 12:21:22 +0200

    Use component output

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,8 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('superheroes');
+
+   printSuperhero(hero: any) 
+   { console.log(hero); // print the received event object. 
+    }
 }
--- src/app/superheroes-material-design/superhero-profile/superhero-profile.ts
+++ src/app/superheroes-material-design/superhero-profile/superhero-profile.ts
@@ -1,36 +1,46 @@
 import { Component } from '@angular/core';
 import SuperheroText from '../utilities/superhero-text';
-import { Input } from '@angular/core';
+import { Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-superhero-profile',
   standalone: false,
   //template: `
- // <div>test superhero profile works!</div>
- // `,
+  // <div>test superhero profile works!</div>
+  // `,
   templateUrl: './superhero-profile.html',
   styleUrl: './superhero-profile.scss',
   providers: [SuperheroText]
 })
 export class SuperheroProfile {
 
-   @Input() name: string="";
-   //@Input() firstAppearance: string="";
-   @Input("lives-in") livesIn: string="";
+  @Input() name: string = "";
+  //@Input() firstAppearance: string="";
+  @Input("lives-in") livesIn: string = "";
 
-    // create a private field 
-    private _firstAppearance: number=0; 
-    // use a public setter 
-    @Input() set firstAppearance(val: number) 
-    { if (val > 1950)
-      { this._firstAppearance = val;   } 
-      else 
-        { console.error("Superhero is too old"); 
+  // create a private field 
+  private _firstAppearance: number = 0;
+  // use a public setter 
+  @Input() set firstAppearance(val: number) {
+    if (val > 1950) { this._firstAppearance = val; }
+    else {
+      console.error("Superhero is too old");
 
-        } 
-      } 
-      // getter for the use of template. 
-      get firstAppearance() 
-      { return this._firstAppearance; }
+    }
+  }
+  @Output() flyWithSuperhero = new EventEmitter<any>();
+  // getter for the use of template. 
+  get firstAppearance() { return this._firstAppearance; }
+
+
+  returnSuperheroData() {
+    let hero = {
+      name: this.name,
+      livesIn: this.livesIn,
+      firstAppearance: this._firstAppearance
+    };
+    this.flyWithSuperhero.emit(hero);
+  }
 
 }
